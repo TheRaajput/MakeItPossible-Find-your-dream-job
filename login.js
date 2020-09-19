acd287b06baff7911e75fea66e84343ec95675d0
TheRaajput <yasharth.cse@gmail.com>
2020-09-20 00:09:13 +0530

    init

---
+++ login.js
@@ -0,0 +1,7 @@
+let getstarted = document.getElementById("get_started");
+let getsignin = document.getElementById("getsignin")
+
+function toregistration(){
+    getstarted.style.visibility = "hidden"
+    getsignin.style.visibility = "visible"
+}
