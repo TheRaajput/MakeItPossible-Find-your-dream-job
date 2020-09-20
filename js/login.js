905f88a5c904c0466cf56287577e2d9f0817f1f2
TheRaajput <yasharth.cse@gmail.com>
2020-09-20 22:23:18 +0530

    init

---
+++ js/login.js
@@ -0,0 +1,5 @@
+let getstarted = document.getElementById("get_started");
+let box = document.querySelector(".form-box");
+
+function toregistration(){
+}
--- login.js
+++
@@ -1,7 +0,0 @@
-let getstarted = document.getElementById("get_started");
-let getsignin = document.getElementById("getsignin")
-
-function toregistration(){
-    getstarted.style.visibility = "hidden"
-    getsignin.style.visibility = "visible"
-}
