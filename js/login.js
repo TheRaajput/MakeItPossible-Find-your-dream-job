1e11a1d4178fa1b33f2e9d97949680febb61e614
TheRaajput <yasharth.cse@gmail.com>
2020-09-26 19:09:18 +0530

    login

--- js/login.js
+++ js/login.js
@@ -5,17 +5,17 @@ function toregistration(){
 }
 
 
-let logIn = document.getElementById("login");
-let signIn = document.getElementById("register");
+let logIn = document.getElementById("login-page");
+let signIn = document.getElementById("register-page");
 let btntoggle = document.getElementById("my-butn");
 
-function login(){
+function loginToggle(){
     logIn.style.left = "50px"
     signIn.style.left = "450px"
     btntoggle.style.left = "0"
 }
 
-function register(){
+function registerToggle(){
     logIn.style.left = "-400px"
     signIn.style.left = "50px"
     btntoggle.style.left = "110px"
