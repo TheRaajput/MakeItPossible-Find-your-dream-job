452255c70b4f6af701f6cf26545cad9b34bf2bb6
TheRaajput <yasharth.cse@gmail.com>
2020-09-26 11:35:00 +0530

    login page

--- js/login.js
+++ js/login.js
@@ -1,6 +1,23 @@
 let getstarted = document.getElementById("get_started");
-let box = document.querySelector(".form-box");
 
 function toregistration(){
     window.location.href = "login.html"
 }
+
+
+let logIn = document.getElementById("login");
+let signIn = document.getElementById("register");
+let btntoggle = document.getElementById("my-butn");
+
+function login(){
+    logIn.style.left = "50px"
+    signIn.style.left = "450px"
+    btntoggle.style.left = "0"
+}
+
+function register(){
+    logIn.style.left = "-400px"
+    signIn.style.left = "50px"
+    btntoggle.style.left = "110px"
+}
+
